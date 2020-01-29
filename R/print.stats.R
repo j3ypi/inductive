06b4e9f6c3690facebd0995c1dc93fb7b87c51af
j3ypi <j-phnolte@web.com>
2020-01-29 14:40:03 +0100

    print documentation

--- R/print.stats.R
+++ R/print.stats.R
@@ -1,5 +1,8 @@
 #' Generic S3 print method for objects of class stats
 #' 
+#' @param x The object to be printed.
+#' @param ... further arguments to be passed to or from methods.
+#' 
 #' @export
 print.stats <- function(x, ...) { 
   options(scipen = 10)
